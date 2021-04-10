116253b23cfd9e976132bb819268f88a82115182
mnpkoerner <mnpkoerner@gmail.com>
2021-04-09 23:13:00 -0500

    added framer motion to dependencies

--- src/Components/Spike/Spike.js
+++ src/Components/Spike/Spike.js
@@ -3,6 +3,7 @@ import {useHistory} from 'react-router-dom'
 //spike goal: build a 3x3 grid
 //place two checkers on the grid
 //console.log when one checker jumps another
+//use Framer Motion
 
 export default function Spike() {
     const history = useHistory()
